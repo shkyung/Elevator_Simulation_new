4903bf13689ff649185d224925409d54e989dc69
shkyung <sunny.kyung88@gmail.com>
2019-04-10 23:12:25 +0900

    refine floorBtnView, add global API in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Controller from "./js/controllers/controller.js";
-import FloorBtnView from "./js/views/floorBtnView.js";
+import FloorBtnView, {floorBtnViewEl} from "./js/views/floorBtnView.js";
 
 (function () {
     let simulationBtn = document.getElementById("simulation");
 
     simulationBtn.addEventListener("click", onClickStart);
+
+    registerGlobalApi();
 
     function onClickStart() {
         let elevatorNum = +document.getElementById("elevatorInput").value,
@@ -23,5 +25,25 @@ import FloorBtnView from "./js/views/floorBtnView.js";
 
         // ElevatorView가 많아지고 로드할게 많아진다면 비동기 로드로 하고 아래코드는 _.defer()를 사용해서 처리
         FloorBtnView.initialize(floorNum);
+    }
+
+    function registerGlobalApi() {
+        /**
+         * @description 해당 층의 버튼이 활성화되었는지 반환한다.
+         * @param {number} floor
+         * @returns {boolean}
+         */
+
+        window.isFloorBtnActive = function(floor){
+            return floorBtnViewEl.querySelector("[value='" + floor + "']").classList.contains("disable");
+        };
+
+        /**
+         * @description 해당 층의 버튼을 활성화 시킨다.
+         * @param {number} floor
+         */
+        window.activeFloorBtn = function (floor) {
+            floorBtnViewEl.querySelector("[value='" + floor + "']").classList.remove("disable");
+        };
     }
 })();
--- src/js/views/floorBtnView.js
+++ src/js/views/floorBtnView.js
@@ -1,66 +1,19 @@
-// 전역 함수 선언
-// import Elevator from "../models/elevator";
-// import Constants from "../utils/const";
-//
-// var isFloorBtnActive, activeFloorBtn;
-//
-// define(function (require) {
-//   "use strict";
-//
-//   // import
-//   var $ = require("jquery"),
-//     Controller = require("../controllers/controller");
-//
-//   //private variables
-//   var $el;
-//
-//   /**
-//    * @description 해당 층의 버튼이 활성화되었는지 반환한다.
-//    * @param {number} floor
-//    * @returns {boolean}
-//    */
-//   isFloorBtnActive =  function (floor) {
-//     return !$el.find("[value='" + floor + "']").hasClass("disable");
-//   };
-//
-//   /**
-//    * @description 해당 층의 버튼을 활성화 시킨다.
-//    * @param {number} floor
-//    */
-//   activeFloorBtn = function (floor) {
-//     $el.find("[value='" + floor + "']").removeClass("disable");
-//   };
-//
-//   function disableFloorBtn(floor) {
-//     $el.find("[value='" + floor + "']").addClass("disable");
-//   }
-//
-//   function onClickFloorBtn (e) {
-//     var $target = $(e.currentTarget);
-//
-//     if (!$target.hasClass("disable")) {
-//       Controller.request(+$target.attr("value"), disableFloorBtn);
-//     }
-//   }
-//
-//   return {
-//     initialize: function (floorNum) {
-//       $el = $(".floorBtnView");
-//
-//       for (var i = floorNum; i > 0; i--) {
-//         var $floor = $("<div></div>").addClass("floor"),
-//           $floorBtn = $("<div></div>").addClass("floorBtn").attr("value", i).text(i + "층");
-//
-//         $floorBtn.on("click", onClickFloorBtn);
-//
-//         $floor.append($floorBtn);
-//         $el.append($floor);
-//       }
-//     }
-//   }
-// });
+import Controller from "../controllers/controller.js";
 
-let floorBtnView = document.getElementById("floorBtnView");
+export let floorBtnViewEl = document.getElementById("floorBtnView");
+
+let disableFloorBtn = function (floor) {
+    floorBtnViewEl.querySelector("[value='" + floor + "']").classList.add("disable");
+};
+
+let onClickFloorBtn = function (e) {
+    let target = e.currentTarget;
+
+    if (!target.classList.contains("disable")) {
+        Controller.request(+target.getAttribute("value"), disableFloorBtn);
+    }
+};
+
 class FloorBtnView {
     // constructor과 같은 레벨에 선언하면 prototype 함수로 들어감.
     constructor() {
@@ -68,18 +21,20 @@ class FloorBtnView {
             FloorBtnView.instance = this;
         }
 
-        this.initialize = function(floorNum) {
-            for(let i = floorNum; i > 0; i--) {
+        this.initialize = function (floorNum) {
+            for (let i = floorNum; i > 0; i--) {
                 let floor = document.createElement("div"),
                     floorBtn = document.createElement("div");
 
                 floor.classList.add("floor");
                 floorBtn.classList.add("floorBtn");
                 floorBtn.setAttribute("value", i);
-                floorBtn.innerText = i+"층";
+                floorBtn.innerText = i + "층";
 
                 floor.appendChild(floorBtn);
-                floorBtnView.appendChild(floor);
+                floorBtnViewEl.appendChild(floor);
+
+                floorBtn.addEventListener("click", onClickFloorBtn);
             }
         };
 
