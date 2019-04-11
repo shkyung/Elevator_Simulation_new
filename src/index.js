53f90ce3508cff8ae0440cc26a2eb17373582480
shkyung <sunny.kyung88@gmail.com>
2019-04-12 01:32:24 +0900

    remove useless polyfill. npm polyfill is better to use

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import App from "./js/controllers/app.js";
-import polyfill1 from "./js/utils/customEventPolyfill.js";
-import polyfill2 from "./js/utils/arrayMehtodPolyfill.js";
 import "ie-array-find-polyfill";
 import "custom-event-polyfill";
 import style from "./css/app.css";
