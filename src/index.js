02a1a9aacb06dc5ae46cec778f1ceda22904b7ca
shkyung <sunny.kyung88@gmail.com>
2019-04-12 01:11:25 +0900

    each two npm polyfill, file polyfill for IE test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import App from "./js/controllers/app.js";
-import polyfill from "../utils/customEventPolyfill.jsPolyfill.js";
-import style from "../../css/app.css";
-import polyfill2 from "../utils/arrayMethodPolyfill.js";
+import polyfill1 from "./js/utils/customEventPolyfill.js";
+import polyfill2 from "./js/utils/arrayMehtodPolyfill.js";
+import "ie-array-find-polyfill";
+import "custom-event-polyfill";
+import style from "./css/app.css";
 
 App.init();
--- src/js/utils/arrayMethodPolyfill.js
+++ src/js/utils/arrayMethodPolyfill.js
@@ -1,16 +1,25 @@
 export default function ArrayMethodPolyfill() {
+    if (!Array.prototype.find) {
+        Array.prototype.find = function(predicate) {
+            if (this == null) {
+                throw new TypeError('Array.prototype.find called on null or undefined');
+            }
+            if (typeof predicate !== 'function') {
+                throw new TypeError('predicate must be a function');
+            }
+            let list = Object(this);
+            let length = list.length >>> 0;
+            let thisArg = arguments[1];
+            let value;
 
-    if (typeof window.CustomEvent === "function") return false;
-
-    function CustomEvent(event, params) {
-        params = params || {bubbles: false, cancelable: false, detail: undefined};
-        var evt = document.createEvent('CustomEvent');
-        evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
-        return evt;
+            for (let i = 0; i < length; i++) {
+                value = list[i];
+                if (predicate.call(thisArg, value, i, list)) {
+                    return value;
+                }
+            }
+            return undefined;
+        };
     }
-
-    CustomEvent.prototype = window.Event.prototype;
-
-    window.CustomEvent = CustomEvent;
 }
 ArrayMethodPolyfill();
